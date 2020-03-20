ec81bcf3d3f8b6bd92b711a4608e7ec5e188b2e2
Toyo <709292639@qq.com>
2020-03-20 21:11:00 +0800

    生命周期/函数/变量/计算属性定义

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>about页面</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>{{msg}}</div>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+// 全局导航守卫改造
+import router from "@/router";
+router.beforeEach((to: any, from: any, next: any) => {
+  // console.info(to);
+  next();
+});
+
+// 装饰器 - 注册组件
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+
+export default class About extends Vue {
+  // private - 定义一个私有变量（等同于在vue的data中的变量）
+  private msg: string = "私有变量msg";
+}
+</script>
+
+<style scoped lang="less">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" @click="clickPhoto" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>私有变量：{{msg}}</div>
+    <div>计算属性：{{computedMsg}}</div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
+<script lang='ts'>
+// lang = 'ts' 注意设定
+import { Vue, Component } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+// 注册组件
+@Component({
   components: {
     HelloWorld
+  }
+})
+export default class Home extends Vue {
+  // 定义私有变量
+  private msg: number = 100;
+
+  // 生命周期函数
+  private mounted() {
+    console.info("mounted生命周期");
+  }
+
+  // computed 计算属性
+  private get computedMsg() {
+    return "computed" + this.msg * 2;
+  }
+
+  // method 定义函数
+  private clickPhoto() {
+    alert(`点击图片触发了 clickPhoto 函数`);
   }
 }
 </script>
